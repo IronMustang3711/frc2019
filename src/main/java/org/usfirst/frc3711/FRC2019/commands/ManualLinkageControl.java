65b44acd4d008b8b9066d0226af813d45cc52221
Jason Markham <jason@millsmart.com>
2019-01-31 15:46:07 -0800

    manual control of linkage subsystems with joystick

---
+++ src/main/java/org/usfirst/frc3711/FRC2019/commands/ManualLinkageControl.java
@@ -0,0 +1,37 @@
+package org.usfirst.frc3711.FRC2019.commands;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import org.usfirst.frc3711.FRC2019.Robot;
+import org.usfirst.frc3711.FRC2019.subsystems.LinkageSubsystem;
+
+public class ManualLinkageControl extends Command {
+
+  private final LinkageSubsystem subsystem;
+
+  public ManualLinkageControl(LinkageSubsystem subsystem) {
+    this.subsystem = subsystem;
+    requires(subsystem);
+    requires(Robot.chassis);
+  }
+
+
+  @Override
+  protected void execute() {
+    double output = Robot.oi.joystick1.getY();
+
+    output *= -0.5;
+
+    SmartDashboard.putNumber(subsystem.getName() + " output",output);
+
+    subsystem.talon.set(ControlMode.PercentOutput,output);
+
+    Robot.chassis.drive(0,0); // just in case
+  }
+
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+}
