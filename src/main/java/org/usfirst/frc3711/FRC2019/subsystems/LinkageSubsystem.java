236a73a118714a47839af4a7660cd8d75fb2a874
Trout Lake School First Robotics Team <robotics@tlschool.net>
2019-01-31 17:26:24 -0800

    basic telemetry for linkage subsystems

--- src/main/java/org/usfirst/frc3711/FRC2019/subsystems/LinkageSubsystem.java
+++ src/main/java/org/usfirst/frc3711/FRC2019/subsystems/LinkageSubsystem.java
@@ -1,7 +1,9 @@
 package org.usfirst.frc3711.FRC2019.subsystems;
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public abstract class LinkageSubsystem extends Subsystem {
 
@@ -10,6 +12,26 @@ public abstract class LinkageSubsystem extends Subsystem {
   public LinkageSubsystem(String name, int talonID){
     super(name);
     this.talon = new WPI_TalonSRX(talonID);
+    configureTalon();
+  }
+
+  void configureTalon(){
+    talon.configFactoryDefault();
+    talon.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
+    talon.setSensorPhase(true);
+
+  }
+  void telemetry(){
+    double busVoltage = talon.getBusVoltage();
+    double motorOutputVoltage = talon.getMotorOutputVoltage();
+    double motorOutputPercent = talon.getMotorOutputPercent();
+    double position = talon.getSelectedSensorPosition();
+    double velocity = talon.getSelectedSensorVelocity();
+    SmartDashboard.putNumber(getName() + ": Bus Voltage", busVoltage);
+    SmartDashboard.putNumber(getName() + ": Output Voltage", motorOutputVoltage);
+    SmartDashboard.putNumber(getName() + ": Output Percent", motorOutputPercent);
+    SmartDashboard.putNumber(getName() + ": Position", position);
+    SmartDashboard.putNumber(getName() + ": Velocity", velocity);
   }
 
 //  @Override
