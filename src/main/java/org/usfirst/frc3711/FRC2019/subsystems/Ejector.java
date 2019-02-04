d7bdfc86b75f109f3e1726b9dbcfbf77efb082d8
Trout Lake School First Robotics Team <robotics@tlschool.net>
2019-02-04 11:48:43 -0800

    add ejector subsystem

---
+++ src/main/java/org/usfirst/frc3711/FRC2019/subsystems/Ejector.java
@@ -0,0 +1,36 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc3711.FRC2019.subsystems;
+
+import org.usfirst.frc3711.FRC2019.TalonID;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * Add your docs here.
+ */
+public class Ejector extends LinkageSubsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  public Ejector(){
+    super(Ejector.class.getSimpleName(),TalonID.EJECTOR.getId());
+  }
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+
+  @Override
+  public void periodic() {
+    super.periodic();
+    SmartDashboard.putNumber("Ejector Position", talon.getSensorCollection().getQuadraturePosition());
+  }
+}
