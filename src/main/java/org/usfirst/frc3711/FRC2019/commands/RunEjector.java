a98145b46397c9333a7edc3a1568a5815f0160b3
Trout Lake School First Robotics Team <robotics@tlschool.net>
2019-02-06 16:07:25 -0800

    intake & ejector. untested

---
+++ src/main/java/org/usfirst/frc3711/FRC2019/commands/RunEjector.java
@@ -0,0 +1,43 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc3711.FRC2019.commands;
+
+import org.usfirst.frc3711.FRC2019.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class RunEjector extends Command {
+  public RunEjector() {
+  requires(Robot.ejector);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    Robot.ejector.run();
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+    Robot.ejector.stop();
+  }
+
+
+}
