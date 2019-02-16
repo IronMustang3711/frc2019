43d3030dac7e1930eecf50e79a345dc2054d6c1a
Trout Lake School First Robotics Team <robotics@tlschool.net>
2019-02-16 13:03:24 -0800

    added jog buttons

--- src/main/java/org/usfirst/frc3711/deepspace/OI.java
+++ src/main/java/org/usfirst/frc3711/deepspace/OI.java
@@ -39,6 +39,8 @@ public class OI {
   final JoystickButton doglegDownButton;
   final JoystickButton rearJackDownButton;
   final JoystickButton liftingMechanismsUp;
+  final JoystickButton jogUp;
+  final JoystickButton jogDown;
 
 
   public OI() {
@@ -47,6 +49,9 @@ public class OI {
 
     stow = new JoystickButton(joystick1, 7);
     stow.whenPressed(new RobotPoser(RobotPose.STOW));
+
+    jogUp = new JoystickButton(joystick1, 9);
+    jogDown = new JoystickButton(joystick1, 10);
 
 
 //    elevator = new JoystickButton(joystick1, 5);
