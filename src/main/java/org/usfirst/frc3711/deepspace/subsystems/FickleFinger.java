ab0deed56334f796a4d8bf4ae8877256c8fa182f
jasonnn <jasonmarkham@gmail.com>
2019-02-16 10:16:49 -0800

    fickle finger configuration. add testing buttons to shuffleboard

--- src/main/java/org/usfirst/frc3711/deepspace/subsystems/FickleFinger.java
+++ src/main/java/org/usfirst/frc3711/deepspace/subsystems/FickleFinger.java
@@ -8,6 +8,8 @@
 package org.usfirst.frc3711.deepspace.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.InstantCommand;
 import org.usfirst.frc3711.deepspace.TalonID;
 import org.usfirst.frc3711.deepspace.talon.TalonUtil;
 
@@ -21,8 +23,71 @@ public class FickleFinger extends TalonSubsystem {
     super(FickleFinger.class.getSimpleName(), TalonID.FICKLE_FINGER.getId());
 
     talonTelemetry = TalonUtil.closedLoopTelemetry(this);
+
+    tab.add(new Command("closed loop control(Position)") {
+
+      {
+        requires(FickleFinger.this);
+      }
+
+      @Override
+      protected void execute() {
+        // if (ntClosedLoopEnabled.getBoolean(false)) {
+        talon.set(ControlMode.Position, ntSetpoint.getDouble(0.0));
+        //}
+      }
+
+      @Override
+      protected boolean isFinished() {
+        return false;
+      }
+    });
+
+    tab.add(new Command("Run Forward") {
+      {
+        requires(FickleFinger.this);
+      }
+      @Override
+      protected boolean isFinished() {
+        return false;
+      }
+
+      @Override
+      protected void execute() {
+        super.execute();
+        setMotorOutput(0.5);
+      }
+
+      @Override
+      protected void end() {
+        super.end();
+        disable();
+      }
+    });
+
+    tab.add(new InstantCommand("Reset Encoder") {
+      {
+        requires(FickleFinger.this);
+        setRunWhenDisabled(true);
+      }
+
+      @Override
+      protected void execute() {
+        talon.setSelectedSensorPosition(0);
+        talon.getSensorCollection().setQuadraturePosition(0, 50);
+
+      }
+
+    });
   }
 
+  @Override
+  void configureTalon() {
+    super.configureTalon();
+    talon.setSensorPhase(true);
+    talon.config_kP(0,1.5);
+
+  }
 
   public boolean isRunning() {
     return talon.getMotorOutputPercent() != 0;
