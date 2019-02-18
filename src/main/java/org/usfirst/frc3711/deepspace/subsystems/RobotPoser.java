73be41e93501df6d6c16c612816530e8697bccaf
jasonnn <jasonmarkham@gmail.com>
2019-02-18 01:20:45 -0800

    RobotPoser WIP

--- src/main/java/org/usfirst/frc3711/deepspace/subsystems/RobotPoser.java
+++ src/main/java/org/usfirst/frc3711/deepspace/subsystems/RobotPoser.java
@@ -3,6 +3,8 @@ package org.usfirst.frc3711.deepspace.subsystems;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.InstantCommand;
+import edu.wpi.first.wpilibj.command.MyCommandGroup;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
 import org.usfirst.frc3711.deepspace.RobotPose;
 import org.usfirst.frc3711.deepspace.commands.sequences.*;
 
@@ -14,7 +16,7 @@ import java.util.function.Supplier;
 
 public class RobotPoser extends RobotSubsystem {
 
-  private RobotPose pose = RobotPose.STOW;
+  private RobotPose currentPose = RobotPose.STOW;
 
   private Map<RobotPose, Supplier<Command>> factories;
 
@@ -31,39 +33,60 @@ public class RobotPoser extends RobotSubsystem {
     //TODO: only return transitions that are known to work
     return valid;
   }
+
+  private Map<RobotPose, Supplier<Command>> makeFactoryMap(){
+    Map<RobotPose, Supplier<Command>> factoryMap = new EnumMap<>(RobotPose.class);
+    factoryMap.put(RobotPose.STOW,this::stowCommandForCurrentPose );
+
+    factoryMap.put(RobotPose.FUEL_0, HatchFuel0::new);
+    factoryMap.put(RobotPose.FUEL_1, HatchFuel1::new);
+    factoryMap.put(RobotPose.FUEL_2, HatchFuel2::new);
+
+    factoryMap.put(RobotPose.HATCH_0, HatchPanel0::new);
+    factoryMap.put(RobotPose.HATCH_1,HatchPanel1::new);
+    factoryMap.put(RobotPose.HATCH_2,HatchPanel2::new);
+
+    factoryMap.put(RobotPose.FUEL_PICKUP,LoadingStationFuel::new);
+
+    factoryMap.put(RobotPose.GROUND_PICKUP,GroundPickup::new);
+
+    assert factoryMap.keySet().equals(EnumSet.allOf(RobotPose.class))
+        : "missing a robot pose command factory";
+
+    return factoryMap;
+  }
+
   public RobotPoser(){
     super(RobotPoser.class.getSimpleName());
 
-    factories = new EnumMap<>(RobotPose.class);
-    factories.put(RobotPose.STOW,this::stowCommandForCurrentPose );
+    factories = makeFactoryMap();
 
-    factories.put(RobotPose.FUEL_0, HatchFuel0::new);
-    factories.put(RobotPose.FUEL_1, HatchFuel1::new);
-    factories.put(RobotPose.FUEL_2, HatchFuel2::new);
+    var container = tab.getLayout("Poses", BuiltInLayouts.kList); //TODO figure out grid constraints
 
-    factories.put(RobotPose.HATCH_0, HatchPanel0::new);
-    factories.put(RobotPose.HATCH_1,HatchPanel1::new);
-    factories.put(RobotPose.HATCH_2,HatchPanel2::new);
-
-    factories.put(RobotPose.FUEL_PICKUP,LoadingStationFuel::new);
-
-    factories.put(RobotPose.GROUND_PICKUP,GroundPickup::new);
-
-    assert factories.keySet().equals(EnumSet.allOf(RobotPose.class))
-        : "missing a robot pose command factory";
+    for(var p : RobotPose.values()) {
+      //TODO change to CommandGroup to see completion/debugging?
+      container.add(new InstantCommand(p.name(),() -> setPose(p)));
+    }
 
   }
 
+  @SuppressWarnings("WeakerAccess")
   public boolean isPosing(){
     return currentCommand.isRunning();
   }
 
   public void setPose(RobotPose newPose){
     if(isPosing()){
-      DriverStation.reportWarning("Attempting to change poses while already posing",false);
+      DriverStation.reportError("Attempting to change poses while already posing",false);
       return; // TODO:can make delayed command?
     }
-
+    if(!VALID_TRANSITIONS.get(currentPose).contains(newPose)){
+      DriverStation.reportError("Invalid pose transition",false);
+      return;
+    }
+    currentPose = newPose;
+    currentCommand= factories.get(newPose).get();
+    currentCommand.start();
   }
 
   private static EnumSet<RobotPose> upperLevelPoses = EnumSet.of(
@@ -71,7 +94,7 @@ public class RobotPoser extends RobotSubsystem {
       RobotPose.FUEL_1,RobotPose.FUEL_2);
 
   private Command stowCommandForCurrentPose(){
-    return null; //TODO: implement!
+    return new Stow(); //TODO: upper level & Loading station
   }
 
 }
