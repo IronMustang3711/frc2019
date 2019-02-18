66d005961b5d5f6ebcc5b2c387eebb1f34e67b8d
jasonnn <jasonmarkham@gmail.com>
2019-02-17 21:49:42 -0800

    Create new Stow Command. preparing to remove RobotPoser command

---
+++ src/main/java/org/usfirst/frc3711/deepspace/commands/sequences/Stow.java
@@ -0,0 +1,22 @@
+package org.usfirst.frc3711.deepspace.commands.sequences;
+
+import edu.wpi.first.wpilibj.command.MyCommandGroup;
+import org.usfirst.frc3711.deepspace.Robot;
+import org.usfirst.frc3711.deepspace.commands.util.MotionMagicSetpoint;
+
+public class Stow extends MyCommandGroup {
+  public Stow(){
+    super(Stow.class.getSimpleName());
+
+    addParallel(new MotionMagicSetpoint("Wrist Home", Robot.wrist,0));
+    addParallel(new MotionMagicSetpoint("Arm Home",Robot.arm,0));
+    addParallel(new MotionMagicSetpoint("Elevator Home",Robot.elevator,0));
+  }
+
+  @Override
+  protected void end() {
+    Robot.elevator.talon.neutralOutput();
+    Robot.arm.talon.neutralOutput();
+    Robot.wrist.talon.neutralOutput();
+  }
+}
