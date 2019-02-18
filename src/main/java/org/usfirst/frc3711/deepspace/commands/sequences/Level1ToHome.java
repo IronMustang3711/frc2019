a5b8160a227424abea13041e645438b174c1bcfb
jasonnn <jasonmarkham@gmail.com>
2019-02-17 19:22:40 -0800

    Level1ToHome working for HatchFuel1

--- src/main/java/org/usfirst/frc3711/deepspace/commands/sequences/Level1ToHome.java
+++ src/main/java/org/usfirst/frc3711/deepspace/commands/sequences/Level1ToHome.java
@@ -1,17 +1,27 @@
 package org.usfirst.frc3711.deepspace.commands.sequences;
 
-import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.MyCommandGroup;
 import org.usfirst.frc3711.deepspace.Robot;
+import org.usfirst.frc3711.deepspace.commands.util.Commands;
 import org.usfirst.frc3711.deepspace.commands.util.MotionMagicSetpoint;
 
-public class Level1ToHome extends CommandGroup {
+public class Level1ToHome extends MyCommandGroup {
   public Level1ToHome(){
     super(Level1ToHome.class.getSimpleName());
 
-    addSequential(new MotionMagicSetpoint("Elevator Up", Robot.elevator,4000,2.5));
-    addParallel(new MotionMagicSetpoint("Wrist Home",Robot.wrist,10,2.5));
-    addParallel(new MotionMagicSetpoint("Arm Home",Robot.arm,10,2.5));
-    addSequential(new MotionMagicSetpoint("Elevator Home",Robot.arm,10,2.5));
+    var elevatorUp = new MotionMagicSetpoint("Elevator Up", Robot.elevator,5000);
+    addParallel(elevatorUp);
+    addSequential(Commands.delayUntil(() -> Math.abs(Robot.elevator.talon.getClosedLoopError()) < 500));
+
+    var wristHome = new MotionMagicSetpoint("Wrist Home",Robot.wrist,0);
+    addParallel(wristHome);
+    addSequential(Commands.delayUntil(()-> wristHome.getMotionProgress() > 0.5));
+
+    var armHome = new MotionMagicSetpoint.ArmSetpoint("Arm Home",0);
+    addParallel(armHome);
+
+    addSequential(Commands.delayUntil(()-> armHome.getMotionProgress() > 0.5 && wristHome.getMotionProgress() > 0.6));
+    addSequential(new MotionMagicSetpoint("Elevator Home",Robot.elevator,10));
   }
 
 }
