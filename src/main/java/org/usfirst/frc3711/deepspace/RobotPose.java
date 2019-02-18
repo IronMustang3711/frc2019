7753715447aa1e0779e106cccff6a4bffc50133e
jasonnn <jasonmarkham@gmail.com>
2019-02-17 22:09:42 -0800

    remove setpoints from RobotPose. add remaining poses.

--- src/main/java/org/usfirst/frc3711/deepspace/RobotPose.java
+++ src/main/java/org/usfirst/frc3711/deepspace/RobotPose.java
@@ -1,19 +1,13 @@
 package org.usfirst.frc3711.deepspace;
 
 public enum RobotPose {
-  GROUND_PICKUP(0, 0, 0),
-  LOWER_HATCHPANEL(0, 0, 0),
-  STOW(400, 0, 40),
-  HATCH_0(2000, -10, 600),
-  HATCH_1(4600, -10, 600);
-
-  RobotPose(double elevator, double arm, double wrist) {
-    this.elevatorSetpoint = elevator;
-    this.armSetpoint = arm;
-    this.wristSetpoint = wrist;
-  }
-
-  public final double elevatorSetpoint;
-  public final double armSetpoint;
-  public final double wristSetpoint;
+  STOW,
+  GROUND_PICKUP,
+  HATCH_0,
+  FUEL_0,
+  FUEL_PICKUP,
+  HATCH_1,
+  FUEL_1,
+  HATCH_2,
+  FUEL_2;
 }
