c8b0f6d01a93164cb0a8b63e7b0494bdf5cfb828
jasonnn <jasonmarkham@gmail.com>
2019-02-18 19:04:01 -0800

    Hatchpanel1 tweaks

--- src/main/java/org/usfirst/frc3711/deepspace/commands/sequences/HatchPanel1.java
+++ src/main/java/org/usfirst/frc3711/deepspace/commands/sequences/HatchPanel1.java
@@ -2,6 +2,7 @@ package org.usfirst.frc3711.deepspace.commands.sequences;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.MyCommandGroup;
+import edu.wpi.first.wpilibj.command.WaitForChildren;
 import edu.wpi.first.wpilibj.shuffleboard.EventImportance;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import org.usfirst.frc3711.deepspace.Robot;
@@ -20,8 +21,8 @@ public class HatchPanel1 extends MyCommandGroup {
 
     // elevator up:
     addParallel(new MotionMagicSetpoint("Wrist Vertical", Robot.wrist, 90), elevatorUpTimeout);
-    addParallel(new MotionMagicSetpoint("Arm Vertical", Robot.arm, 40), elevatorUpTimeout);
-    addSequential(new MotionMagicSetpoint("bring elevator up", Robot.elevator, elevatorPosition, 1.5) {
+    addParallel(new MotionMagicSetpoint("Arm Vertical", Robot.arm, 0), elevatorUpTimeout);
+    addSequential(new MotionMagicSetpoint("bring elevator up", Robot.elevator, elevatorPosition) {
       @Override
       protected boolean isFinished() {
         return super.isFinished();
@@ -35,7 +36,7 @@ public class HatchPanel1 extends MyCommandGroup {
     addParallel(new MotionMagicSetpoint("Hold Elevator Position",
         Robot.elevator, elevatorPosition, armOutTimeout));
     //	addSequential(new MotionMagicSetpoint("Wrist Down",Robot.wrist,-2949),armOutTimeout);
-    addSequential(new MotionMagicSetpoint("Arm Out", Robot.arm, 3200, 3.0) {
+    addParallel(new MotionMagicSetpoint("Arm Out", Robot.arm, 3200, 3.0) {
       @Override
       protected boolean isFinished() {
         return super.isFinished();
@@ -43,8 +44,8 @@ public class HatchPanel1 extends MyCommandGroup {
         // 		|| Math.abs(subsystem.talon.getClosedLoopError()) < 150;
       }
     });
-    addSequential(new MotionMagicSetpoint("Wrist Down", Robot.wrist, -1800), armOutTimeout);
-
+    addParallel(new MotionMagicSetpoint("Wrist Down", Robot.wrist, -1800), armOutTimeout);
+    addSequential(new WaitForChildren());
     addSequential(new MotionMagicSetpoint("Elevator Down", Robot.elevator, -7688));
 
   }
