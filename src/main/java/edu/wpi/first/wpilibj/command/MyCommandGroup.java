350c1c3b860ee6c8156ebda01b6ec46985b77327
jasonnn <jasonmarkham@gmail.com>
2019-02-18 20:03:02 -0800

    fix MyCommandGroup tracing

--- src/main/java/edu/wpi/first/wpilibj/command/MyCommandGroup.java
+++ src/main/java/edu/wpi/first/wpilibj/command/MyCommandGroup.java
@@ -1,6 +1,7 @@
 package edu.wpi.first.wpilibj.command;
 
 import java.lang.reflect.Field;
+import java.util.List;
 import java.util.Vector;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
@@ -10,116 +11,106 @@ public class MyCommandGroup extends CommandGroup {
   @SuppressWarnings("WeakerAccess")
   public Consumer<CharSequence> debugSink = System.out::println;
 
+  enum Reflection {
+    INSTANCE;
+      Field childrenField;
+      Field commandsField;
+      Field entryCommandField;
+      Reflection(){
+        try {
+          childrenField = CommandGroup.class.getDeclaredField("m_children");
+          childrenField.setAccessible(true);
+          commandsField = CommandGroup.class.getDeclaredField("m_commands");
+          commandsField.setAccessible(true);
 
-  private static Field childrenField;
-  private static Field commandsField;
+          Class<?> entryClass = Class.forName("edu.wpi.first.wpilibj.command.CommandGroup$Entry");
+          entryCommandField = entryClass.getDeclaredField("m_command");
+          entryCommandField.setAccessible(true);
+        } catch (ClassNotFoundException | NoSuchFieldException e) {
+          throw new RuntimeException(e);
+        }
+      }
 
-  static {
-    reflective_stuff();
+    /**
+     *
+     * @return Vector of Entries
+     */
+    Vector<?> getCommandsVector(CommandGroup group){
+      try {
+        return (Vector<?>) commandsField.get(group);
+      } catch (IllegalAccessException e) {
+        throw new RuntimeException(e);
+      }
+    }
+
+    /**
+     *
+     * @return Vector of Entries
+     */
+    Vector<?> getChildrenVector(CommandGroup group){
+      try {
+        return (Vector<?>) childrenField.get(group);
+      } catch (IllegalAccessException e) {
+        throw new RuntimeException(e);
+      }
+    }
+
+    Command extractCommandFromEntry(Object entry){
+      try {
+        return (Command) entryCommandField.get(entry);
+      } catch (IllegalAccessException e) {
+        throw new RuntimeException(e);
+      }
+    }
+
+    List<Command> getCommands(CommandGroup cg){
+      return getCommandsVector(cg).stream().map(this::extractCommandFromEntry).collect(Collectors.toList());
+    }
+    List<Command> getChildren(CommandGroup cg){
+      return getChildrenVector(cg).stream().map(this::extractCommandFromEntry).collect(Collectors.toList());
+    }
+
   }
-
-  private Vector<?> children;
-  private Vector<?> commands;
-  private String prevRunningCommands = "";
 
   @SuppressWarnings("unused")
   public MyCommandGroup() {
-    do_reflection();
   }
 
   public MyCommandGroup(String name) {
     super(name);
-    do_reflection();
   }
 
-  private static void reflective_stuff() {
-    try {
-      childrenField = CommandGroup.class.getDeclaredField("m_children");
-      childrenField.setAccessible(true);
-      commandsField = CommandGroup.class.getDeclaredField("m_commands");
-      commandsField.setAccessible(true);
+   private String debugString(){
+    var commands = Reflection.INSTANCE.getCommands(this);
+    var children = Reflection.INSTANCE.getChildren(this);
 
-      Class<?> entryClass = Class.forName("edu.wpi.first.wpilibj.command.CommandGroup$Entry");
-      Field entryCommandField = entryClass.getDeclaredField("m_command");
-      entryCommandField.setAccessible(true);
-    } catch (ClassNotFoundException | NoSuchFieldException e) {
-      throw new RuntimeException(e);
-    }
+    var runningCommands = commands.stream().filter(Command::isRunning).collect(Collectors.toList());
+
+
+
+    var runningChildrenStr = runningCommands.stream()
+                                 .filter(children::contains)
+                                 .map(Command::getName)
+                                 .collect(Collectors.joining(", ", ".[", "]"));
+
+    var runningSiblingsStr = runningCommands.stream()
+                                 .filter(c -> !children.contains(c))
+                                 .map(Command::getName)
+                                 .collect(Collectors.joining(", ","..[","]"));
+
+    return getName()+ "[Running] " + runningChildrenStr +" "+ runningSiblingsStr;
   }
 
-  private void do_reflection() {
-    try {
-      children = (Vector) childrenField.get(this);
-      commands = (Vector) commandsField.get(this);
-    } catch (IllegalAccessException e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  @SuppressWarnings("WeakerAccess")
-  public String debugString(){
-    //TODO: commands is a vector of "Entry"'s , no commands
-    //TODO: need to extract Command from Entry
-//    var runningCommands = commands.stream()
-//                              .map(Command.class::cast)
-//                              .filter(Command::isRunning)
-//                              .collect(Collectors.toList());
-//
-//    var childCommands = children.stream()
-//                            .map(Command.class::cast)
-//                            .collect(Collectors.toSet());
-//
-//    var runningChildrenStr = runningCommands.stream()
-//                                 .filter(childCommands::contains)
-//                                 .map(Command::getName)
-//                                 .collect(Collectors.joining(", ", "@[", "]"));
-//
-//    var runningSiblingsStr = runningCommands.stream()
-//                                 .filter(c -> !childCommands.contains(c))
-//                                 .map(Command::getName)
-//                                 .collect(Collectors.joining(", "));
-
-    return getName();// + "[Running] " + runningChildrenStr + runningSiblingsStr;
-  }
+  private String prevDebugString = "";
 
   @Override
   void _execute() {
     super._execute();
-
-
-
-//    var runningChildren = commands.stream()
-//                              .map(Command.class::cast)
-//                              .filter(Command::isRunning).filter(childCommands::contains)
-//                              .sorted(Comparator.comparing(SendableBase::getName))
-//                              .map(Command::getName)
-//                              .collect(Collectors.joining(", ", "@[", "]"));
-//    var childCommands = children.stream()
-//                            .map(Command.class::cast)
-//                            .collect(Collectors.toSet());
-//
-//    var runningCommandsSorted =
-//        commands.stream()
-//            .map(Command.class::cast)
-//            .filter(Command::isRunning)
-//            .sorted((a, b) -> childCommands.contains(a)
-//                                  && !childCommands.contains(b)
-//                                  ? -1
-//                                  : a.getName().compareTo(b.getName()))
-//            .map(Command::getName)
-//            .collect(Collectors.joining(", "));
-
-    //.collect(Collectors.groupingBy(childCommands::contains,);
-    //.map(command -> childCommands.contains(command) ? "." + command.getName() : ".." + command.getName())
-    //.collect(Collectors.joining(", "));
-
     var debugStr = debugString();
 
-    if (!prevRunningCommands.equals(debugStr)) {
-      debugSink.accept(getName() + "[Running]" + debugStr);
-      prevRunningCommands = debugStr;
-      //debugSink.accept(getName() + "[Running]: " + runningCommandsSorted);
-     // prevRunningCommands = runningCommandsSorted;
+    if (!prevDebugString.equals(debugStr)) {
+      debugSink.accept(debugStr);
+      prevDebugString = debugStr;
     }
 
   }
---
+++ src/test/java/edu/wpi/first/wpilibj/command/MyCommandGroupTest.java
@@ -0,0 +1,77 @@
+package edu.wpi.first.wpilibj.command;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class MyCommandGroupTest {
+
+static class TestCommand extends Command {
+
+  TestCommand(String name){ super(name);}
+  int numIsFinishedCallsAllowed = 3;
+  int isFinishedCalls = 0;
+  @Override
+  protected boolean isFinished() {
+    return ++isFinishedCalls > numIsFinishedCallsAllowed;
+  }
+}
+
+
+  @Test(expected = Test.None.class)
+  public void testReflection() {
+    var reflection = MyCommandGroup.Reflection.INSTANCE;
+
+    var cg = new CommandGroup("testing");
+    cg.addParallel(new TestCommand("1"));
+    cg.addSequential(new TestCommand("2"));
+
+
+    var childrenVec = reflection.getChildrenVector(cg);
+    assertNotNull(childrenVec);
+
+    var commandsVec = reflection.getCommandsVector(cg);
+    assertNotNull(commandsVec);
+
+    var commands = reflection.getCommands(cg);
+    assertEquals(2,commands.size());
+
+    var children = reflection.getChildren(cg);
+    assertEquals(0,children.size());
+  }
+
+  @Test
+  public void testMyCommandGroupDebugString() {
+    List<CharSequence> debugMessages = new ArrayList<>();
+
+    var cg = new MyCommandGroup("testing");
+    cg.setRunWhenDisabled(true);
+    cg.debugSink = debugMessages::add;
+
+    TestCommand c1 = new TestCommand("1");
+    cg.addParallel(c1);
+    TestCommand c2 = new TestCommand("2");
+    cg.addSequential(c2);
+
+    cg.start();
+
+    Scheduler.getInstance().enable();
+    Scheduler.getInstance().run();
+
+    assertEquals("[]",debugMessages.toString());
+
+    Scheduler.getInstance().run();
+
+    assertEquals("[testing[Init], testing[Running] .[1] ..[2]]",debugMessages.toString());
+
+    Scheduler.getInstance().run();
+    Scheduler.getInstance().run();
+    Scheduler.getInstance().run();
+
+    assertEquals("[testing[Init], testing[Running] .[1] ..[2], testing[Running] .[] ..[], testing[End]]",debugMessages.toString());
+
+  }
+}
