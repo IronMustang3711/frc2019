a23a92055a8f7058cb69eaaead149b7e7acabf07
jasonnn <jasonmarkham@gmail.com>
2019-03-08 08:24:34 -0800

    continuous jog tweaks

--- src/main/java/org/usfirst/frc3711/deepspace/commands/JogElevatorContinuously.java
+++ src/main/java/org/usfirst/frc3711/deepspace/commands/JogElevatorContinuously.java
@@ -6,29 +6,44 @@ import org.usfirst.frc3711.deepspace.commands.util.TalonSubsystemCommand;
 
 public class JogElevatorContinuously extends TalonSubsystemCommand {
   private final boolean up;
-  public JogElevatorContinuously(boolean up){
+  private static final int UP_SETPOINT = 8000;
+  private static final int DOWN_SETPOINT = 0;//-8000;
+  private static final int STOP_DISTANCE = 600;
+  private int position;
+
+  public JogElevatorContinuously(boolean up) {
     super("JogElevatorContinuously" + (up ? "Up" : "Down"), Robot.elevator);
     this.up = up;
   }
 
   @Override
+  protected void initialize() {
+    super.initialize();
+    subsystem.talon.selectProfileSlot(0, 0); //MM slot
+
+    subsystem.talon.set(ControlMode.MotionMagic, up ? UP_SETPOINT : DOWN_SETPOINT);
+  }
+
+  @Override
   protected void execute() {
     super.execute();
-    subsystem.talon.set(ControlMode.MotionMagic, up? 10000:-8000);
+    position = subsystem.talon.getSelectedSensorPosition();
 
   }
 
   @Override
   protected void end() {
     super.end();
-    int position = subsystem.talon.getSelectedSensorPosition();
-    subsystem.talon.set(ControlMode.MotionMagic, position);
+    subsystem.talon.selectProfileSlot(1, 0); //position slot
+    subsystem.talon.set(ControlMode.Position, position);
+
   }
 
   @Override
   protected boolean isFinished() {
     int position = subsystem.talon.getSelectedSensorPosition();
-    return super.isFinished()||(up&&position>=10000)||(!up&&position<=-8000);
-
+    return super.isFinished()
+           || (up && position >= UP_SETPOINT - STOP_DISTANCE)
+           || (!up && position <= DOWN_SETPOINT + STOP_DISTANCE);
   }
 }
