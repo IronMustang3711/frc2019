25f6cfa5a81bc105312d13cb0e7d8c7757f7fffb
Trout Lake School First Robotics Team <robotics@tlschool.net>
2019-03-08 18:56:01 -0800

    create a "hold" command

---
+++ src/main/java/org/usfirst/frc3711/deepspace/commands/MaintainSetpoint.java
@@ -0,0 +1,35 @@
+package org.usfirst.frc3711.deepspace.commands;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
+import org.usfirst.frc3711.deepspace.commands.util.TalonSubsystemCommand;
+import org.usfirst.frc3711.deepspace.subsystems.TalonSubsystem;
+
+public class MaintainSetpoint extends TalonSubsystemCommand {
+    double target;
+    public MaintainSetpoint(TalonSubsystem subsystem){
+        super("Hold",subsystem);
+    }
+
+    @Override
+    protected void initialize() {
+        super.initialize();
+        subsystem.talon.selectProfileSlot(1, 0);
+        target = subsystem.talon.getClosedLoopTarget();
+    }
+
+    @Override
+    protected void execute() {
+        super.execute();
+        subsystem.talon.set(ControlMode.Position, target);
+    }
+
+    @Override
+    protected void end() {
+        super.end();
+        subsystem.talon.selectProfileSlot(1, 0);
+    }
+
+
+
+}
