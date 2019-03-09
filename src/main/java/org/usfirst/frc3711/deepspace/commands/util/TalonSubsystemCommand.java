7a44b0b61f6fc26e972b0ae06a3f8c2cee264288
jasonnn <jasonmarkham@gmail.com>
2019-03-09 10:25:05 -0800

    make TalonSubsystemCommand abstract. make ctor public

--- src/main/java/org/usfirst/frc3711/deepspace/commands/util/TalonSubsystemCommand.java
+++ src/main/java/org/usfirst/frc3711/deepspace/commands/util/TalonSubsystemCommand.java
@@ -1,8 +1,9 @@
 package org.usfirst.frc3711.deepspace.commands.util;
 
+import edu.wpi.first.wpilibj.command.AbstractCommand;
 import org.usfirst.frc3711.deepspace.subsystems.TalonSubsystem;
 
-public class TalonSubsystemCommand extends AbstractCommand {
+public abstract class TalonSubsystemCommand extends AbstractCommand {
   protected final TalonSubsystem subsystem;
 
   public TalonSubsystemCommand(String name, TalonSubsystem subsystem, double timeout) {
@@ -10,7 +11,7 @@ public class TalonSubsystemCommand extends AbstractCommand {
     this.subsystem = subsystem;
   }
 
-  protected TalonSubsystemCommand(String name, TalonSubsystem subsystem) {
+  public TalonSubsystemCommand(String name, TalonSubsystem subsystem) {
     super(name, subsystem);
     this.subsystem = subsystem;
   }
